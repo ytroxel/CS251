d108dffc715873c2f07c2c9c0728159ea29eb17e
yada <35783905+ytroxel@users.noreply.github.com>
2018-02-07 14:19:45 -0800

    Create 2.16.cpp

---
+++ hw2/2.16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int a;
+ int b;
+ 
+ cout << "enter two integers: " << endl;
+ cin >> a >> b;
+ cout << endl << a + b << endl;
+ cout << a * b << endl;
+ cout << a - b << endl;
+ cout << a / b << endl;
+}
