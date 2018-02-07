297567a3d8721d27d05cfe9a1377fdaabda788ee
yada <35783905+ytroxel@users.noreply.github.com>
2018-02-07 14:16:09 -0800

    Create 2.5.cpp

---
+++ hw2/2.5.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int x = 0;
+ int y = 0;
+ int z = 0;
+ int result = 0;
+
+ cout << "Enter 3 integers: " << endl;
+ cin >> x >> y >> z;
+ result = x * y * z;
+ cout << x << "*" << y << "*" << z << "=" << result << endl;
+ 
+}
