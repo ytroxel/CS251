aad3651b19737e413dcaa64fded42424668aa7d1
Yada T <35783905+ytroxel@users.noreply.github.com>
2018-02-09 19:02:34 -0800

    Update 2.28.cpp

--- hw2/2.28.cpp
+++ hw2/2.28.cpp
@@ -7,7 +7,7 @@ int main()
   int x;
   cin >> x;
   cout << x / 10000 << "   ";
-  cout << x / 1000 % 100 % 10 << "   ";
+  cout << x / 1000 % 10 << "   ";
   cout << x / 100 % 10 << "   ";
   cout << x / 10 % 10 << "   ";
   cout << x / 1 % 10 << "   " << endl;
