f31d29ad8a25e1ad6657831d8f21c22e4e698c95
Yada T <35783905+ytroxel@users.noreply.github.com>
2018-03-05 19:53:37 -0800

    Create 3.13.h

---
+++ hw3/3.13.h
@@ -0,0 +1,8 @@
+/* (Invoice Class) Create a class called Invoice that a hardware store might use to represent an invoice for an item sold at 
+the store. An Invoice should include four data members—a part num- ber (type string), a part description (type string), a 
+quantity of the item being purchased (type int) and a price per item (type int). [Note: In subsequent chapters, we’ll use 
+numbers that contain decimal points (e.g., 2.75)—called floating-point values—to represent dollar amounts.] Your class should 
+have a constructor that initializes the four data members. A constructor that receives multiple arguments is defined with the 
+form:
+ClassName( TypeName1 parameterName1, TypeName2 parameterName2, ... )*/
+
