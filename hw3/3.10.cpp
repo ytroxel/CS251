bf20e61aec5cb9c4d865fb0bec366f66b4b3dc98
Yada T <35783905+ytroxel@users.noreply.github.com>
2018-03-07 14:01:42 -0800

    Update 3.10.cpp

--- hw3/3.10.cpp
+++ hw3/3.10.cpp
@@ -1,10 +1,34 @@
 //invoice file
 #include <iostream>
-#include "invoice.hpp"
+#include "Invoice.hpp"
 #include <string>
 using namespace std;
 
 int main()
 {
-
+Account invoice1(string name, int number, string descrip, int quantity)
+  
+  cout << "Enter tool name: \n";
+  getline(cin, name);
+  Invoice.setName(name);
+  
+  cout << "Enter tool number: \n";
+  cin >> number;
+  Invoice.setNumber(number);
+  
+  cout << "Enter tool description: \n";
+  getline(cin, descrip)
+  Invoice.setDescription(descrip);
+  
+  cout << "Enter tool quantity: \n";
+  cin >> quantity;
+  Invoice.setQuantity(quantity);
+  
+  cout << "Tool entered: \n";
+  cout << Invoice.getName << endl;
+  cout << Invoice.getNumber << endl;
+  cout << Invoice.getDescription << endl;
+  cout << Invoice.getQuantity << endl;
+  
+  return 0;
 }
