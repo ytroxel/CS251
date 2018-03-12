d884018ec3230ae3860a1702960dc26ceb5ad62f
Yada T <35783905+ytroxel@users.noreply.github.com>
2018-03-11 19:49:36 -0700

    Update 3.12.cpp

--- hw3/3.12.cpp
+++ hw3/3.12.cpp
@@ -4,3 +4,38 @@ members. For the purpose of this exercise, assume that the values provided for t
 value si in the range 1- 12; if it isn't, set the moth to 1. Provide a set and get function for each data member. Provide a member function
 displayDate that displays the month, day and year separated by forward slashes(/). Write a test program that demonstrates class Date's 
 capabilities*/
+
+#include <iostream>
+#include <string>
+#include "Date.hpp"
+using namespace std;
+
+int main()
+{
+Date object;
+int day, month, year;
+
+cout << "Enter the day of the Date:\n" << "DD\r";
+cin >> day;
+object.setDay(day);
+//cout << object.getDay() << endl;
+
+cout << "Enter the month of the Date:\n" << "MM\r";
+cin >> month;
+object.setMonth(month);
+//cout << object.getMonth() << endl;
+
+if (month <= 1)
+	object.setMonth(1);
+else if (month > 12)
+	object.setMonth(1);
+
+cout << "Enter the year of the Date:\n" << "YYYY\r";
+cin >> year;
+object.setYear(year);
+
+cout << object.getMonth() << "/" << object.getDay() << "/" 
+<< object.getYear() << endl;
+
+
+}
